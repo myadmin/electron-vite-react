b8f14f09f774423b58b654a26e93d5ef37bb61d0
zhouwei <zhou21192345@163.com>
2022-05-31 23:32:38 +0800

    feat: 新增下载图书相关代码

--- packages/main/event.ts
+++ packages/main/event.ts
@@ -1,4 +1,4 @@
-import { ipcMain } from 'electron';
+import { ipcMain, shell } from 'electron';
 import path from 'path';
 import dayjs from 'dayjs';
 import { search, searchDetail, parseUrl } from './search';
@@ -40,47 +40,45 @@ ipcMain.on('parseBook', async (event, arg) => {
     }
 });
 
-// 新开一个隐形的窗口，并进行图书下载
+// 图书下载
 ipcMain.on('downloadBookFile', async (event, arg) => {
-    // console.log('arg', arg);
-    const {
-        downurl: url,
-        bookId,
-        file_name: filename,
-        file_size: filesize,
-    } = arg;
+    console.log('arg', arg);
+    const { downurl: url, bookId, file_name: filename, file_size: filesize } = arg;
     const currentDay = dayjs().format('YYYY-MM-DD');
-    const filePath = path.join(__dirname, `../../download/`, currentDay);
+    const dirPath = path.join(__dirname, `../../download/`);
+    const filePath = path.join(dirPath, currentDay);
     fse.ensureDirSync(filePath);
-
-    // TODO
-    // 应该根据不同的bookId新建隐藏的窗口进行下载，不会互相影响
 
     // 下载配置项
     const option = {
         filename,
         dir: filePath,
-        onDone: (info: any) => {
+        onDone: () => {
             // console.log('done', info);
             event.sender.send('downloadDone', {
                 bookId,
                 done: true,
-                path: info.path,
+                path: filePath
             });
         },
         onError: (err: any) => {
             console.log('error', err);
         },
         onProgress: (curr: number, total: number) => {
-            // console.log('progress', ((curr / total) * 100).toFixed(2) + '%');
             const progress = ((curr / total) * 100).toFixed(2) + '%';
             event.sender.send('downloadProgress', {
                 bookId,
                 progress,
                 filename,
-                filesize,
+                filesize: (filesize / 1024 / 1024).toFixed(2) + 'MB',
+                dirPath,
             });
         },
     };
     dl(url, option);
+});
+
+// 打开文件所在位置
+ipcMain.on('openFilePath', async (event, args) => {
+    shell.openPath(args);
 });
--- packages/renderer/src/pages/Download/index.tsx
+++ packages/renderer/src/pages/Download/index.tsx
@@ -1,8 +1,111 @@
-import React from 'react'
+import { FC, useEffect, useRef, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Button, Tabs } from 'antd';
+import './indes.scss';
 
-const Download = () => {
+interface DownloadBookProps {
+  id?: string;
+  progress?: string;
+  filename: string;
+  filesize: string;
+  filepath?: string;
+  done?: boolean;
+  dirPath: string;
+}
+
+const { TabPane } = Tabs;
+
+const Download: FC = () => {
+  const location: any = useLocation();
+  const [books, setBooks] = useState<DownloadBookProps[]>([]);
+  const bookList = useRef<DownloadBookProps[]>([]);
+
+  useEffect(() => {
+    console.log('location', location);
+    // 监听页面跳转获取到的数据，给主进程发起下载通知
+    if (location?.state && location?.state?.code === 200) {
+      // 下载文件
+      window.ipcRenderer.send('downloadBookFile', location?.state);
+    }
+  }, [location?.state]);
+
+  useEffect(() => {
+    // 监听下载进度
+    window.ipcRenderer.on('downloadProgress', (event, args: any) => {
+      console.log('args', args.progress);
+      let allBook: any = [].concat(books as any);
+      const filter = allBook.filter((book: DownloadBookProps) => book.id !== args.bookId);
+      if (!filter.length) {
+        allBook.push({
+          id: args.bookId,
+          progress: args.progress,
+          filename: args.filename,
+          filesize: args.filesize,
+          done: false,
+        });
+      }
+      bookList.current = allBook;
+      setBooks(allBook);
+    });
+
+    return () => {
+      window.ipcRenderer.removeAllListeners('downloadProgress');
+    };
+  }, []);
+
+  useEffect(() => {
+    // 监听文件是否下载完毕
+    window.ipcRenderer.on('downloadDone', (event, args) => {
+      let allBook: any = [].concat(bookList.current as any);
+      // console.log('allBook', allBook);
+      allBook.map((book: any) => {
+        if (book.id === args.bookId) {
+          book.done = true;
+          book.dirPath = args.path;
+        }
+        return book;
+      });
+      // console.log('allBook', allBook);
+      setBooks(allBook);
+    });
+
+    return () => {
+      window.ipcRenderer.removeAllListeners('downloadDone');
+    };
+  }, []);
+
+  // 查看文件位置
+  const hanldeViewFile = (path: string) => {
+    console.log('文件地址', path);
+    window.ipcRenderer.send('openFilePath', path);
+  }
+
   return (
-    <div>Download</div>
+    <Tabs defaultActiveKey="1">
+      <TabPane tab="正在下载" key="1">
+        <div className="download-ing">
+          {books.length ? (books || []).map(book => (
+            <div className='download-item' key={book.id}>
+              <div className="progress">
+                <div className="progress-item" style={{ width: book.progress }} />
+              </div>
+              <div className="download-status">
+                <div className="status-left">
+                  <p className='name'>{book.filename}</p>
+                  <p className="size">{book.filesize}</p>
+                </div>
+                <div className="status-right">
+                  {book.done ? <Button type='link' onClick={() => hanldeViewFile(book.dirPath)}>查看文件</Button> : book.progress}
+                </div>
+              </div>
+            </div>
+          )) : '暂无下载任务'}
+        </div>
+      </TabPane>
+      <TabPane tab="下载完成" key="2">
+        Content of Tab Pane 2
+      </TabPane>
+    </Tabs>
   )
 }
 
